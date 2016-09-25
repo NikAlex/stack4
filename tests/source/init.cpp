c2e68566af3cdbdc5649ccca8740389ada6aefd0
NikAlex <alez90@yandex.ru>
2016-09-25 20:43:44 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -29,3 +29,25 @@ SCENARIO("const", "[constr]"){
   stack<int> s2=s1;
   REQUIRE(s1.count()==s2.count());
  }
+
+SCENARIO("empty", "[empty]"){
+  stack<int> s1;
+  s1.push(1);
+  REQUIRE(s1.empty()==false);
+}
+
+SCENARIO("empty2", "[empty2]"){
+  stack<int> s1;
+  s1.push(1);
+  s1.pop();
+  REQUIRE(s1.empty()==true);
+}
+
+SCENARIO("empty3", "[empty3]"){
+  stack<int> s1;
+  s1.push(1);
+  s1.push(2);
+  s1.pop();
+  
+  REQUIRE(s1.empty()==false);
+}
