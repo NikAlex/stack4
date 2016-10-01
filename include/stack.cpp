08709d2982fa772c08155144b98b932e79e46589
NikAlex <alez90@yandex.ru>
2016-10-01 23:32:49 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -56,10 +56,12 @@ inline auto stack<T>::count() const noexcept->size_t {
 
 template<typename T>
 inline auto stack<T>::pop() -> T {
-	if (count_ == 0) {
-		throw std::range_error("Empty stack");
-	}
+	if (count_ == 0)
+	throw std::range_error("Empty stack");
 	return array_[--count_];
 }
 
-
+template <typename T>
+inline auto stack<T>::top() const->T& {
+	if (count_ == 0) throw std::logic_error("Empty stack");
+	return ptr_[count_];
