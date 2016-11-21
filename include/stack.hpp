055cf4a5f38c9772769051d40989b9221c49e3f5
NikAlex <alez90@yandex.ru>
2016-11-21 17:11:37 +0400

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -3,8 +3,6 @@
 #pragma once
 #include <string>
 
-#define MULTIPLIER 2 
-
 
 
 
@@ -105,7 +103,7 @@ size_t stack<T>::array_size() const {
 template <typename T>
 void stack<T>::push(T const &obj) {
 	if (allocator<T>::count_ == allocator<T>::array_size_) {
-		size_t array_size = allocator<T>::array_size_ * MULTIPLIER + (allocator<T>::array_size_ == 0);
+		size_t array_size = allocator<T>::array_size_ * 2 + (allocator<T>::array_size_ == 0);
 		stack<T> temp(array_size);
 		while (temp.count() < allocator<T>::count_) temp.push(allocator<T>::array_[temp.count()]);
 		allocator<T>::swap(temp);
