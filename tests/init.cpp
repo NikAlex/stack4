f9f72f496c52f5f5294cdb2b0505c00648e9c299
NikAlex <alez90@yandex.ru>
2016-12-15 22:25:08 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,6 +1,8 @@
 #include "stack.hpp"
 #include <catch.hpp>
 #include <iostream>
+#include <thread>
+#include<mutex>
 using namespace std;
  
 SCENARIO("count", "[count]"){
@@ -13,55 +15,30 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-
 }
 
-SCENARIO("top", "[top]"){
+SCENARIO("pop", "[pop]"){
   stack<int> s;
   s.push(1);
-  s.push(2);
-  s.push(3);
-  s.pop();
-
-  REQUIRE(s.top()==2);
+  REQUIRE(*(s.pop())==1);	
+  REQUIRE(s.count()==0);
 }
-SCENARIO("operprisv", "[operprisv]"){
-  stack<int> s1;
-  s1.push(1);
+
+SCENARIO("prisv", "[prisv]"){
+  stack<int> s;
+  s.push(1);
   stack<int> s2;
-  s2=s1;
-  REQUIRE(s1.count()==s2.count());
-}
-
-SCENARIO("const", "[constr]"){
-  stack<int> s1;
-  s1.push(1);
-  stack<int> s2=s1;
-  REQUIRE(s1.count()==s2.count());
+  s2=s;
+  REQUIRE(s.count()==1);
 }
 
 SCENARIO("empty", "[empty]"){
-  stack<int> s1;
+  stack<int> s1, s2;
   s1.push(1);
-  REQUIRE(s1.empty()==false);
+  REQUIRE(!s1.empty());
+  REQUIRE(s2.empty());
 }
 
-SCENARIO("empty2", "[empty2]"){
-  stack<int> s1;
-  s1.push(1);
-  s1.pop();
-  REQUIRE(s1.empty()==true);
-}
-
-SCENARIO("empty3", "[empty3]"){
-  stack<int> s1;
-  s1.push(1);
-  s1.push(2);
-  s1.pop();
-  s1.top();
-  
-  REQUIRE(s1.empty()==false);
-}
 
 SCENARIO("threads", "[threads]"){
   stack<int> s;
